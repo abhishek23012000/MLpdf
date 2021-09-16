3b82def348c9d20fbad78f59662bd8a8f40c1f73
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-09-16 16:06:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var image = new Image();
 var i = 1;
-image.src = `https://dd17.codetantra.com/bigbluebutton/presentation/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/31d8e119078b9280e784de8b63f0e198c8b48dce-1631183248539/svg/${i}`;
+image.src = `https://dd19.codetantra.com/bigbluebutton/presentation/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/31d8e119078b9280e784de8b63f0e198c8b48dce-1631788091314/svg/${i}`;
 $("#video").html(image);
+
 document.onkeydown = function (event) {
   switch (event.keyCode) {
     case 37:
       i = i - 1;
-      image.src = `https://dd17.codetantra.com/bigbluebutton/presentation/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/31d8e119078b9280e784de8b63f0e198c8b48dce-1631183248539/svg/${i}`;
+      image.src = `https://dd19.codetantra.com/bigbluebutton/presentation/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/31d8e119078b9280e784de8b63f0e198c8b48dce-1631788091314/svg/${i}`;
       $("#video").html(image);
       break;
     case 39:
       i = i + 1;
-      image.src = `https://dd17.codetantra.com/bigbluebutton/presentation/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/31d8e119078b9280e784de8b63f0e198c8b48dce-1631183248539/svg/${i}`;
+      image.src = `https://dd19.codetantra.com/bigbluebutton/presentation/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/31d8e119078b9280e784de8b63f0e198c8b48dce-1631788091314/svg/${i}`;
       $("#video").html(image);
       break;
   }
@@ -19,7 +20,7 @@ document.onkeydown = function (event) {
 
 function othername() {
   i = document.getElementById("userInput").value;
-  image.src = `https://dd17.codetantra.com/bigbluebutton/presentation/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/a5dad1558b319ad357dd8d3751f42d3506698058-1631183183174/31d8e119078b9280e784de8b63f0e198c8b48dce-1631183248539/svg/${i}`;
+  image.src = `https://dd19.codetantra.com/bigbluebutton/presentation/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/19ee9a94d37a2bf3dc5e8b6295e013ed014c9ec2-1631787976134/31d8e119078b9280e784de8b63f0e198c8b48dce-1631788091314/svg/${i}`;
 
   $("#video").html(image);
 }
