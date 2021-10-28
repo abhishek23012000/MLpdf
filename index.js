16961a928ba3f3b52a54747d2c11acf756242ffb
Abhishek raut <56253046+abhishek23012000@users.noreply.github.com>
2021-10-28 15:54:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ function othername() {
  image.src = `https://dd13.codetantra.com/bigbluebutton/presentation/9d179124b5d47830baa3c64b040f258acc56d7fd-1635326871996/9d179124b5d47830baa3c64b040f258acc56d7fd-1635326871996/c009ab1be2fe353805d568eba6099aeaf04efa4e-1635326969406/svg/${i}`;
 
   $("#video").html(image);
+  
 }
